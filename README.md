411c294cb0da953ef71d5bbf6befd6aadeb9b082
davehilditch <dave@affiliatewebdesigners.com>
2022-10-31 10:25:41 +0200

    Updated readme with info, help, issues

--- README.md
+++ README.md
@@ -1,10 +1,44 @@
 # migrate-wordpress
 Migrate WordPress efficiently, quickly, effectively, reliably
 
-Download these files to your DESTINATION server, make sure the migrate.sh file is executable and then run the following command:
+Download these files to your DESTINATION server.
 
-Usage: ./migrate.sh user@originip originfolder destinationfolder [-f]
+`git clone https://github.com/dhilditch/migrate-wordpress`
+
+Then change directory to the migrate-wordpress folder:
+
+`cd migrate-wordpress`
+
+Test you can connect with SSH to your origin server:
+
+`ssh user@originip`
+
+Then run the command. **Be sure you are ready to wipe your destination files and database.** This script overwrites the destination-server database mentioned in the wp-config.php file in your destinationfolder.
+
+#Usage
+`./migrate.sh user@originip originfolder destinationfolder [-f]`
+
+*-f will bypass the confirmation prompt and force the origin backup to run every time (if you re-run this otherwise, it will re-run rsync and the DB restore but not the DB backup)*
 
 This will connect via SSH to your origin server, read your wp-config creds, backup your DB, rsync all the files to your destination, then restore the DB at your destination.
 
 It is smart enough to exclude server-specific files like db.php, advanced-cache.php and more.
+
+This script took 90 minutes to complete flawless migration of my million product server from Digital Ocean to Hetzner
+Run this script from your destination server, make sure you can SSH from your destination server to your origin server first (add your destination server SSH key to your origin authorized_keys file)
+
+#How this migration script works
+There are two scripts, this one that you run on your destination server, then prepare-origin.sh which gets remotely executed on your origin server.
+
+prepare-origin.sh reads your remote wp-config.php, and backs up your db to your webroot folder as a .php file so it can't be read by anyone else
+
+Then this migrate.sh performs an rsync, excluding mu-plugins, wp-includes, wp-admin, wp-config.php, db.php, advanced-cache.php, object-cache.php copying all files to your destination
+
+Finally, it restores your database and then flushes your object cache if you have one
+
+Provided you have SSH access to both servers, this is by far the best migration technique I've found. It's fast, it's reliable, it's secure, and it's free.
+
+#Potential issues
+ - mu-plugins does not get copied, you may need some of these from your origin
+ - wp-config.php does not get copied, you may have some extra customisations in this file you need to copy
+ - it's possible that mysqldump may not be able to write to your webroot folder. If this is the case, you can manually copy the mysqldump.php file to your webroot folder.
--- migrate.sh
+++ migrate.sh
@@ -1,24 +1,31 @@
 # WordPress Site Migration Script 
 # GPL 3 License
 # Copy/re-use as desired, just give me some credit somewhere
-# Created by Dave Hilditch, www.wpintense.com
+# Created by Dave Hilditch, www.wpintense.com, created because I'm fed up with all the other paid migration tools totally failing to work on bigger sites and/or taking forever to complete
+# This script took 90 minutes to complete flawless migration of my million product server from Digital Ocean to Hetzner
 # Run this script from your destination server, make sure you can SSH from your destination server to your origin server first (add your destination server SSH key to your origin authorized_keys file)
 # Usage: ./migrate.sh user@originip originfolder destinationfolder [-f] 
 # -f will bypass the confirmation prompt and force the origin backup to run every time (if you re-run this otherwise, it will re-run rsync and the DB restore but not the DB backup)
+#
+# How this works: There are two scripts, this one that you run on your destination server, then prepare-origin.sh which gets remotely executed on your origin server.
+# prepare-origin.sh reads your remote wp-config.php, and backs up your db to your webroot folder as a .php file so it can't be read by anyone else
+# Then this migrate.sh performs an rsync, excluding mu-plugins, wp-includes, wp-admin, wp-config.php, db.php, advanced-cache.php, object-cache.php copying all files to your destination
+# Finally, it restores your database and then flushes your object cache if you have one
+# Provided you have SSH access to both servers, this is by far the best migration technique I've found. It's fast, it's reliable, it's secure, and it's free.
+# Potential issues:
+#  - mu-plugins does not get copied, you may need some of these from your origin
+#  - wp-config.php does not get copied, you may have some extra customisations in this file you need to copy
+#  - it's possible that mysqldump may not be able to write to your webroot folder. If this is the case, you can manually copy the mysqldump.php file to your webroot folder.
 
 #todo: Add pause before running and overwriting local files and local database - continuing will wipe the local files and database - bypass if -f specified
 echo "Attempting to run: migrate $1 $2 $3 $4"
-
-#FORCE=NO
-#if [ $# -eq 4 ] ; then
-#    FORCE="$4"
-#fi
 
 if ! ssh "$1" 'bash -s' < prepare-origin.sh $2 $4
 then
     echo "Could not connect to origin server. Aborting."
 
     if [ ! -f ~/.ssh/id_rsa.pub ]; then
+        #todo: Add create ssh key prompt
         ssh-keygen -f ~/.ssh/id_rsa.pub
     fi
     echo ""
@@ -56,9 +63,9 @@ else
 fi
 
 echo "Starting rsync to remote server"
-echo "rsync -chavzP --stats  $1:$2 $3 --exclude 'wp-admin' --exclude 'wp-includes' --exclude 'wp-config.php' --exclude 'wp-includes' --exclude 'wp-content/db.php' --exclude 'wp-content/advanced-cache.php' --exclude 'wp-content/debug.log' --exclude 'wp-content/object-cache.php' --exclude 'wp-content/mu-plugins' --exclude 'wp-content/updraft' --exclude 'wp-content/w3tc-config' --exclude 'wp-content/cache'  --exclude 'wp-content/robots.txt'"
+echo "rsync -chavzP --stats  $1:$2 $3 --exclude 'wp-admin' --exclude 'wp-includes' --exclude 'wp-config.php' --exclude 'wp-content/db.php' --exclude 'wp-content/advanced-cache.php' --exclude 'wp-content/debug.log' --exclude 'wp-content/object-cache.php' --exclude 'wp-content/mu-plugins' --exclude 'wp-content/updraft' --exclude 'wp-content/w3tc-config' --exclude 'wp-content/cache'  --exclude 'robots.txt'"
 
-rsync -chavzP --stats  $1:$2 $3 --exclude 'wp-admin' --exclude 'wp-includes' --exclude 'wp-config.php' --exclude 'wp-includes' --exclude 'wp-content/db.php' --exclude 'wp-content/advanced-cache.php' --exclude 'wp-content/debug.log' --exclude 'wp-content/object-cache.php' --exclude 'wp-content/mu-plugins' --exclude 'wp-content/updraft' --exclude 'wp-content/w3tc-config' --exclude 'wp-content/cache'  --exclude 'wp-content/robots.txt'
+rsync -chavzP --stats  $1:$2 $3 --exclude 'wp-admin' --exclude 'wp-includes' --exclude 'wp-config.php' --exclude 'wp-content/db.php' --exclude 'wp-content/advanced-cache.php' --exclude 'wp-content/debug.log' --exclude 'wp-content/object-cache.php' --exclude 'wp-content/mu-plugins' --exclude 'wp-content/updraft' --exclude 'wp-content/w3tc-config' --exclude 'wp-content/cache'  --exclude 'robots.txt'
 
 echo "Rsync Complete"
 echo ""
